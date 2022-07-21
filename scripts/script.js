b15bb561438f232ecca2ada0f61beb6c7b61e0a8
Loh Sze Ying <szeying@Maverickss-MacBook-Pro.local>
2022-07-21 17:36:23 +0800

    calculator with javascript following tutorial completed

--- scripts/script.js
+++ scripts/script.js
@@ -3,58 +3,133 @@
 */
 
 class Calculator {
-    constructor(currentOperandTextElement) {
-        this.currentOperandTextElement = currentOperandTextElement;
-        this.clear();
+  constructor(currentOperandTextElement) {
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
+
+  // append number, including dot (.)
+  // Do not want to append number after computing (unlike the tutorial, there is no clear button here)
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) {
+      return;
     }
-
-    clear() {
-        this.currentOperandTextElement = '';
-        this.operation = undefined;
+    // if justComputed, this.currentOperand = '';
+    if (this.justComputed) {
+      //Do not want to append number, but start a new calculation
+      this.currentOperand = number.toString();
+      this.justComputed = false; //set justComputed to false so user can begin a new computation after computing a result
+    } else {
+      this.currentOperand = this.currentOperand.toString() + number.toString();
     }
+  }
 
-    // append number, including dot (.)
-    appendNumber(number) {
-
+  // + - * /
+  chooseOperation(operation) {
+    if (this.currentOperand === "") {
+      return;
     }
-
-    // + - * /
-    chooseOperation(operation) {
-
+    if (this.previousOperand !== "") {
+      this.compute();
     }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-    // compute the result (after pressing equal button)
-    compute() {
-
+  // compute the result (after pressing equal button)
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) {
+      return;
     }
-    
-    updateDisplay() {
-
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "x":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev / current;
+        break;
+      default:
+        return;
     }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+    this.justComputed = true;
+  }
+
+  getDisplayNumber(number, text) {
+    console.log("number: " + number + ", text: " + text);
+
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+        if (integerDisplay === '') {
+            return text;
+        } else {
+            return integerDisplay;
+        }
+    }
+  }
+
+  //WIP: to make updateDisplay() also display operation if possible, or maybe just continue displaying current operand (like Mac's calculator)
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand,
+      this.operation
+    );
+  }
 }
 
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
 
 const calculator = new Calculator(currentOperandTextElement);
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
-        calculator.updateDisplay();
-    })
-});
-
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText);
-        calculator.updateDisplay();
-    })
-});
-
-equalsButton.addEventListener('click', button => {
-    calculator.compute();
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
 });
